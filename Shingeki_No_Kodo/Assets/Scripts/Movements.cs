6b1a6694798ae4a6cf269f3a73894767a131b55d
ergorilla4e <atwler4@gmail.com>
2023-08-04 17:33:46 +0200

    TileMap_Movements_Collider
    Ho creato la prima ipotetica mappa di gioco, ho aggiunto i vari collider ed ho creato movimenti ed animazioni del personaggio di gioco

---
+++ Shingeki_No_Kodo/Assets/Scripts/Movements.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private void Awake()
+    {
+        this.rb = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    private void OnMovement(Vector2 direction)
+    {
+         if(direction != Vector2.zero)
+        {
+            float length = direction.magnitude;
+
+            if(length > 1 ) 
+            {
+                direction /= length;
+            }
+
+            animator.SetFloat("X", direction.x);
+            animator.SetFloat("Y", direction.y);
+
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2 (h, v);
+
+        OnMovement(direction);
+
+        this.rb.MovePosition(this.rb.position + direction * (Time.deltaTime * this.speed));
+       
+    }
+
+}
